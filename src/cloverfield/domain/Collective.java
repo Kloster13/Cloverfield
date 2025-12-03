9c99b7d8a409a35595e071bdf203f5ab2eb448f8
Chiiriku <224570909+Chiiriku@users.noreply.github.com>
2025-12-03 13:10:51 +0100

    Tjek kommentarer

--- src/cloverfield/domain/Collective.java
+++ src/cloverfield/domain/Collective.java
@@ -1,6 +1,20 @@
 package cloverfield.domain;
 
+import java.time.LocalDate;
+
 public class Collective extends Task
 {
+  public Collective(String description, int pointsGained)
+  {
+    super(description, pointsGained);
+  }
 
+  public Collective(String description, int pointsGained, Resident reservedBy)
+  {
+    super(description, pointsGained, reservedBy);
+  }
+
+  public void completeTask() {
+    super.completeTask(null);
+  }
 }
