952f9d514cc805b9b35f62caf10524519b9b8c66
Chiiriku <224570909+Chiiriku@users.noreply.github.com>
2025-12-10 14:24:52 +0100

    Påbegyndt Cloverfield.fxml og controller - ikke færdig.

--- src/cloverfield/presentation/controllers/CloverfieldController.java
+++ src/cloverfield/presentation/controllers/CloverfieldController.java
@@ -1,5 +1,33 @@
 package cloverfield.presentation.controllers;
 
+import cloverfield.persistence.DataManager;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class CloverfieldController
 {
+
+  public Button showGreenPoints;
+  public Button useGreenPoints;
+  public Button historicUses;
+
+  private DataManager dataManager;
+
+  public void init(DataManager dataManager)
+  {
+    this.dataManager = dataManager;
+  }
+
+  public void onShowCollectiveGrenPointsButton(ActionEvent actionEvent)
+  {
+  }
+
+  public void onUseGreenPointsButton(ActionEvent actionEvent)
+  {
+  }
+
+  public void onShowHistoricUsesButton(ActionEvent actionEvent)
+  {
+
+  }
 }
